0fa4a81fb95721708bb2f41a334dc4cad9e12c3d
rayniel95 <rainyel.ramos@gmail.com>
2023-08-13 13:13:38 -0400

    add delegate vote script

---
+++ scripts/delegateVoteMyERC20Votes.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import { MyERC20Votes, MyERC20Votes__factory } from "../typechain-types";
+import { ethers } from "hardhat";
+
+
+async function main() {
+    const alchemyProvider = new ethers.AlchemyProvider("sepolia", process.env.ALCHEMY_API_KEY);
+    const account = new ethers.Wallet(process.env.toBeDelegatePrivateKey!, alchemyProvider);
+
+    const erc20VotesFactory = new MyERC20Votes__factory(account);
+
+    const contract = erc20VotesFactory.attach(
+        process.env.MyERC20VotesContractAddress as string
+    ) as MyERC20Votes;
+
+    const contractAddress = await contract.delegate(
+        process.env.delegateeAddress!
+    );
+    console.log(`${account.address} delegate to ${process.env.delegateeAddress!}`);
+}
+
+main().then()
