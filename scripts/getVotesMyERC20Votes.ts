52fda1ab7998d943fcb09f29bd1da6c15471a535
rayniel95 <rainyel.ramos@gmail.com>
2023-08-13 16:15:21 -0400

    add get votes script

---
+++ scripts/getVotesMyERC20Votes.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { MyERC20Votes, MyERC20Votes__factory } from "../typechain-types";
+import { ethers } from "hardhat";
+
+//REVIEW - i am curious about the difference between getvotes and balance of
+// and how it is posible to know from erc20votes contract which votes where
+// cast in the tokenized ballot contract
+
+
+async function main() {
+    const alchemyProvider = new ethers.AlchemyProvider("sepolia", process.env.ALCHEMY_API_KEY);
+    const account = new ethers.Wallet(process.env.toGetVotesPrivateKey!, alchemyProvider);
+
+    const myERC20VotesFactory = new MyERC20Votes__factory(account);
+
+    const contract = myERC20VotesFactory.attach(
+        process.env.MyERC20VotesContractAddress! as string
+    ) as MyERC20Votes;
+
+    const contractAddress = await contract.getVotes(
+        account.address,
+    );
+    console.log(`you have ${ethers.formatUnits(contractAddress, 18)} votes`);
+}
+
+main().then()
